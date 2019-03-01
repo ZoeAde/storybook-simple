c5900e4cba1b30737938310c4e07ba0fa2c3f5f6
Zoe Adelman <zoe.a.adelman@gmail.com>
2019-03-01 15:32:37 -0700

    adding button states

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,22 +1,167 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import classNames from 'classnames';
+import { settings } from 'carbon-components';
+import { ButtonTypes } from '../prop-types/types';
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faAngleRight } from '@fortawesome/free-solid-svg-icons'
+// library.add(faAngleRight)
+// <FontAwesomeIcon icon="angle-right" />
 
-// const Button = () => (
-//   <div>
-//     <button color='blue'>Cancel</button>
-//   </div>
-// );
-function Button({ button: onHover, onClick }) {
-  return (
-    <div  >
-      <button className='button' onClick={() => onClick('clicked')} onHover={() => onHover('hover')}>Default</button>
-    </div>
-  );
-}
 
-Button.propTypes = {
-    onClick: PropTypes.func,
-    onHover: PropTypes.func,
-};
+    const { prefix } = settings;
 
-export default Button;
+    const Button = ({
+      children,
+      className,
+      disabled,
+      small,
+      kind,
+      href,
+      tabIndex,
+      type,
+      icon,
+      iconDescription,
+      ...other
+    }) => {
+      const buttonClasses = classNames(className, {
+        [`${prefix}--btn`]: true,
+        [`${prefix}--btn--sm`]: small,
+        [`${prefix}--btn--primary`]: kind === 'primary',
+        [`${prefix}--btn--secondary`]: kind === 'secondary',
+        [`${prefix}--btn--ghost`]: kind === 'ghost',
+        [`${prefix}--btn--icon`]: kind === 'icon',
+      });
+
+      const commonProps = {
+        tabIndex,
+        className: buttonClasses,
+      };
+      const buttonImage = (() => {
+        if (icon) {
+          return (
+            <Icon
+              icon={Object(icon) === icon ? icon : undefined}
+              name={Object(icon) !== icon ? icon : undefined}
+              description={iconDescription}
+              className={`${prefix}--btn__icon`}
+              aria-hidden="true"
+            />
+          );
+        }
+        return null;
+      })();
+
+      const button = (
+        <button
+          {...other}
+          {...commonProps}
+          disabled={disabled}
+          type={type}
+          ref={other.inputref}>
+          {children}
+          {buttonImage}
+        </button>
+      );
+
+      const anchor = (
+        <a
+          {...other}
+          {...commonProps}
+          href={href}
+          role="button"
+          ref={other.inputref}>
+          {children}
+          {buttonImage}
+        </a>
+      );
+
+      return href ? anchor : button;
+    };
+
+    Button.propTypes = {
+      /**
+       * Specify the content of your Button
+       */
+      children: PropTypes.node,
+
+      /**
+       * Specify an optional className to be added to your Button
+       */
+      className: PropTypes.string,
+
+      /**
+       * Specify whether the Button should be disabled, or not
+       */
+      disabled: PropTypes.bool,
+
+      /**
+       * Specify whether the Button should be a small variant
+       */
+      small: PropTypes.bool,
+
+      /**
+       * Specify the kind of Button you want to create
+       */
+      kind: ButtonTypes.buttonKind.isRequired,
+
+      /**
+       * Optionally specify an href for your Button to become an <a> element
+       */
+      href: PropTypes.string,
+
+      /**
+       * Optional prop to specify the tabIndex of the Button
+       */
+      tabIndex: PropTypes.number,
+
+      /**
+       * Optional prop to specify the type of the Button
+       */
+      type: PropTypes.oneOf(['button', 'reset', 'submit']),
+
+      /**
+       * Optional prop to specify the role of the Button
+       */
+      role: PropTypes.string,
+
+      /**
+       * Specify an icon to include in the Button through a string or object
+       * representing the SVG data of the icon
+       */
+      icon: PropTypes.oneOfType([
+        PropTypes.shape({
+          width: PropTypes.string,
+          height: PropTypes.string,
+          viewBox: PropTypes.string.isRequired,
+          svgData: PropTypes.object.isRequired,
+        }),
+        PropTypes.string,
+        PropTypes.node,
+      ]),
+
+      /**
+       * If specifying the `icon` prop, provide a description for that icon that can
+       * be read by screen readers
+       */
+      iconDescription: props => {
+        if (props.icon && !props.iconDescription) {
+          return new Error(
+            'icon property specified without also providing an iconDescription property.'
+          );
+        }
+        return undefined;
+      },
+    };
+
+    Button.defaultProps = {
+      iconDescription: 'Provide icon description if icon is used',
+      tabIndex: 0,
+      type: 'button',
+      disabled: false,
+      small: false,
+      kind: 'primary',
+    };
+
+    export default Button;
--- src/components/Button.stories.js
+++ src/components/Button.stories.js
@@ -1,25 +1,38 @@
-/**
- * Copyright IBM Corp. 2016, 2018
- *
- * This source code is licensed under the Apache-2.0 license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import { withKnobs, boolean, select } from '@storybook/addon-knobs';
+import { settings } from 'carbon-components';
 import Button from './Button';
 
+const { prefix } = settings;
+
+const kinds = {
+  'Primary': 'primary',
+  'Secondary': 'secondary'
+};
 
 const props = {
   regular: () => ({
     className: 'some-class',
+    kind: select('Button kind', kinds, 'primary'),
+    disabled: boolean('Disabled', false),
+    small: boolean('Small', false),
+    // icon: boolean('Icon', false),
     onClick: action('onClick'),
-    onHover: action('onHover'),
+    onFocus: action('onFocus')
+  }),
+  set: () => ({
+    className: 'some-class',
+    disabled: boolean('Disabled (disabled)', false),
+    small: boolean('Small (small)', false),
+    onClick: action('onClick'),
+    onFocus: action('onFocus')
   })
 };
 
 storiesOf('Buttons', module)
+  .addDecorator(withKnobs)
   .add(
     'Default',
     () => {
@@ -31,5 +44,54 @@ storiesOf('Buttons', module)
           </Button>
         </div>
       );
+    },
+    {
+      info: {
+        text: `
+          Buttons are used to initialize an action, either in the background or
+          foreground of an experience.
+
+          There are several kinds of buttons.
+
+          Primary buttons should be used for the principle call to action
+          on the page.
+
+          Secondary buttons should be used for secondary actions on each page.
+
+          Danger buttons should be used for a negative action (such as Delete) on the page.
+
+          Modify the behavior of the button by changing its event properties.
+
+          Small buttons may be used when there is not enough space for a
+          regular sized button. This issue is most found in tables. Small button should have three words
+          or less.
+
+          When words are not enough, icons can be used in buttons to better communicate what the button does. Icons are
+          always paired with text.
+        `,
+      },
     }
-  );
+)
+//   .add(
+//   'Sets of Buttons',
+//   () => {
+//     const setProps = props.set();
+//     return (
+//       <div>
+//         <Button kind="secondary" {...setProps}>
+//           Secondary button
+//         </Button>
+//         <Button kind="primary" {...setProps}>
+//           Primary button
+//         </Button>
+//       </div>
+//     );
+//   },
+//   {
+//     info: {
+//       text: `
+//         When an action required by the user has more than one option, always use a a negative action button (secondary) paired with a positive action button (primary) in that order. Negative action buttons will be on the left. Positive action buttons should be on the right. When these two types buttons are paired in the correct order, they will automatically space themselves apart.
+//       `,
+//     },
+//   }
+// )
